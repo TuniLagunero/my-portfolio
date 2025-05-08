2b599bfa45a55897972666a11919e50d8ba17641
TuniLagunero <talesss.1227@gmail.com>
2025-05-08 23:02:44 +0800

    second commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+//components
+import Nav from "./Nav";
+
 const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto">
-
+            {/* logo */}
             <Link href="/">
             <h1 className="text-4xl font-semibold">
                 Anthony<span className="text-accent">.</span>
             </h1>
             </Link>
+
+
+                {/* desktop nav */}
+                <div className="hidden xl:flex">
+                    <Nav />
+                </div>
             </div>
         </header>
     )
